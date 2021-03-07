afd25c81f1fc234451303042885e65cec7443b14
Nitisha Desai <nitishakdesai@gmail.com>
2021-03-07 12:40:31 -0500

    fix data file

--- public/js/plugins/moral-binary-choice.js
+++ public/js/plugins/moral-binary-choice.js
@@ -387,15 +387,15 @@ new_html += '</div>';
         "action_top": trial.action_top,
         "act_side": trial.act_side,
 
-        "number_fewer": trial.n_fewer,
-        "number_more": trial.n_more,
-        "item_fewer": trial.items.singular,
-        "item_more": trial.items.singular,
+        "number_fewer": n_fewer,
+        "number_more": n_more,
+        "item_fewer": trial.items.F_singular,
+        "item_more": trial.items.M_singular,
 
-        "UL": trial.UL,
-        "LL": trial.LL,
-        "UR": trial.UR,
-        "LR": trial.LR,
+        "UL": UL,
+        "LL": LL,
+        "UR": UR,
+        "LR": LR,
 
         "eyeData": JSON.stringify(eyeData),
         "realOrCatch": trial.realOrCatch,
